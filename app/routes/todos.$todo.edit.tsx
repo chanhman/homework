5ee4eeaba48fa4afd8190150eb0891a01ffa4094
chanhman <hello@chanh.design>
2024-12-25 21:37:30 -0700

    Set pattern for action

--- app/routes/todos.$todo.edit.tsx
+++ app/routes/todos.$todo.edit.tsx
@@ -1,12 +1,49 @@
-import { useParams } from '@remix-run/react';
+import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { Form, useLoaderData, useParams } from '@remix-run/react';
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${params.todo}`
+  );
+  const json = await response.json();
+
+  return json;
+}
 
 export default function TodoEdit() {
+  const loaderData = useLoaderData<typeof loader>();
   const params = useParams();
 
   return (
     <div>
       <h2>Edit {params.todo}</h2>
-      Edit page!
+      <Form method="post">
+        <input type="hidden" name="id" value={loaderData.id} />
+        <label htmlFor="title">Title</label>
+        <br />
+        <input
+          type="text"
+          name="title"
+          id="title"
+          defaultValue={loaderData.title}
+        />
+        <br />
+        <label htmlFor="completed">Completed</label>
+        <input
+          type="checkbox"
+          name="completed"
+          id="completed"
+          defaultChecked={loaderData.completed}
+          value="true"
+        />
+        <button type="submit">Save</button>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  console.log(formData);
+  return null;
 }
