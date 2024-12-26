3c5fdf53cd77cd353974e7cbafefd5c8cc8b2f1d
chanhman <hello@chanh.design>
2024-12-25 23:29:38 -0700

    Add delete

--- app/routes/todos.$todo.edit.tsx
+++ app/routes/todos.$todo.edit.tsx
@@ -1,5 +1,11 @@
 import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
-import { Form, useLoaderData, useParams } from '@remix-run/react';
+import {
+  Form,
+  redirect,
+  useActionData,
+  useLoaderData,
+  useParams,
+} from '@remix-run/react';
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const response = await fetch(`http://localhost:3000/todos/${params.todo}`);
@@ -9,11 +15,13 @@ export async function loader({ params }: LoaderFunctionArgs) {
 }
 
 export default function TodoEdit() {
+  const actionData = useActionData<typeof action>();
   const loaderData = useLoaderData<typeof loader>();
   const params = useParams();
 
   return (
     <div>
+      {actionData?.success && <p>Todo updated!</p>}
       <h2>Edit {params.todo}</h2>
       <Form method="post">
         <input type="hidden" name="id" value={loaderData.id} />
@@ -36,6 +44,9 @@ export default function TodoEdit() {
           value="true"
         />
         <button type="submit">Save</button>
+        <button type="submit" name="command" value="delete">
+          Delete
+        </button>
       </Form>
     </div>
   );
@@ -44,5 +55,29 @@ export default function TodoEdit() {
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   console.log(formData);
-  return null;
+  const command = String(formData.get('command'));
+  const id = String(formData.get('id'));
+  const title = String(formData.get('title'));
+
+  if (command === 'delete') {
+    const response = await fetch(`http://localhost:3000/todos/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      return redirect('/todos');
+    }
+
+    return { success: response.ok };
+  }
+
+  const response = await fetch(`http://localhost:3000/todos/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id, title }),
+  });
+
+  return { success: response.ok };
 }
